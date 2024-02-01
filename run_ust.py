6ad23686a3a46055b0ffa4b5bac8523eef5e488b
lee-jae-hyeong <76590396+lee-jae-hyeong@users.noreply.github.com>
2024-02-01 09:20:41 +0900

    Update run_ust.py

--- run_ust.py
+++ run_ust.py
@@ -82,12 +82,12 @@ if __name__ == '__main__':
 	#get pre-trained tokenizer
 	tokenizer = Tokenizer.from_pretrained(pt_teacher_checkpoint)
 
-	if task_name == "ecommerce_cate":
-		path = "/content/drive/MyDrive/UPET/ecommerce_cate"
-		raw_datasets = load_from_disk(path)
-		train = raw_datasets['train']
-		val = raw_datasets['validation']
-		test= raw_datasets['test']
+	# if task_name == "ecommerce_cate":
+	# 	path = "/content/drive/MyDrive/UPET/ecommerce_cate"
+	# 	raw_datasets = load_from_disk(path)
+	# 	train = raw_datasets['train']
+	# 	val = raw_datasets['validation']
+	# 	test= raw_datasets['test']
 		
 	X_train_all, y_train_all = generate_sequence_data(max_seq_length, "train" ,tokenizer, do_pairwise=do_pairwise)
 
